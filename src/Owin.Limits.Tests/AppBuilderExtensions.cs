556ea0c326464ee86e5a822fee94dcc27667dfe5
Damian Hickey <dhickey@gmail.com>
2014-05-27 06:06:33 +0200

    Code format.

--- src/Owin.Limits.Tests/AppBuilderExtensions.cs
+++ src/Owin.Limits.Tests/AppBuilderExtensions.cs
@@ -1,8 +1,16 @@
 ﻿namespace Owin.Limits
 {
     using AppFunc = System.Func<System.Collections.Generic.IDictionary<string, object>, System.Threading.Tasks.Task>;
-    using MidFunc = System.Func<System.Func<System.Collections.Generic.IDictionary<string, object>, System.Threading.Tasks.Task>, System.Func<System.Collections.Generic.IDictionary<string, object>, System.Threading.Tasks.Task>>;
-    using BuildFunc = System.Action<System.Func<System.Func<System.Collections.Generic.IDictionary<string, object>, System.Threading.Tasks.Task>, System.Func<System.Collections.Generic.IDictionary<string, object>, System.Threading.Tasks.Task>>>;
+    using MidFunc = System.Func<
+        System.Func<System.Collections.Generic.IDictionary<string, object>, System.Threading.Tasks.Task>,
+        System.Func<System.Collections.Generic.IDictionary<string, object>, System.Threading.Tasks.Task>
+        >;
+    using BuildFunc = System.Action<
+        //System.Collections.Generic.IDictionary<string, object>,
+        System.Func<
+            System.Func<System.Collections.Generic.IDictionary<string, object>, System.Threading.Tasks.Task>,
+            System.Func<System.Collections.Generic.IDictionary<string, object>, System.Threading.Tasks.Task>
+        >>;
 
     internal static class AppBuilderExtensions
     {
