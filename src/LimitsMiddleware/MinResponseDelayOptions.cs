29093d4f86a167712a0e91eb4aa6523f514c0ca3
thefringeninja <joao.paul@braganca.name>
2015-03-30 17:10:46 +0200

    middleware to add a minimum delay per response

---
+++ src/LimitsMiddleware/MinResponseDelayOptions.cs
@@ -0,0 +1,55 @@
+﻿namespace LimitsMiddleware
+{
+    using System;
+
+    /// <summary>
+    /// Options for setting a delay to each response
+    /// </summary>
+    public class MinResponseDelayOptions : OptionsBase
+    {
+        private readonly Func<TimeSpan> _getMinDelay;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="minDelay">The delay, in milliseconds</param>
+        public MinResponseDelayOptions(int minDelay)
+            : this(TimeSpan.FromSeconds(minDelay))
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="getMinDelay">returns the delay, in miliseconds</param>
+        public MinResponseDelayOptions(Func<int> getMinDelay)
+            : this(() => TimeSpan.FromMilliseconds(getMinDelay()))
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="minDelay">The delay</param>
+        public MinResponseDelayOptions(TimeSpan minDelay)
+            : this(() => minDelay)
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="getMinDelay">returns the delay</param>
+        public MinResponseDelayOptions(Func<TimeSpan> getMinDelay)
+        {
+            getMinDelay.MustNotNull("getMinDelay");
+
+            _getMinDelay = getMinDelay;
+        }
+
+        public TimeSpan MinDelay
+        {
+            get { return _getMinDelay(); }
+        }
+    }
+}
