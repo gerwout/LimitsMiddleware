f1f62f8b416ee603c25b28e15087d331f4bc6609
Damian Hickey <dhickey@gmail.com>
2015-04-13 17:17:32 +0200

    Import Bert.RateLimiters code.

---
+++ src/LimitsMiddleware.Tests/RateLimiters/FixedTokenBucketTests.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Threading;
+
+namespace Bert.RateLimiters.Tests
+{
+    using FluentAssertions;
+    using Xunit;
+
+    public class FixedTokenBucketTests
+    {
+        private FixedTokenBucket bucket;
+        public const long MAX_TOKENS = 10;
+        public const long REFILL_INTERVAL = 10;
+        public const long N_LESS_THAN_MAX = 2;
+        public const long N_GREATER_THAN_MAX = 12;
+        private const int CUMULATIVE = 2;
+
+        public FixedTokenBucketTests()
+        {
+            bucket = new FixedTokenBucket(MAX_TOKENS, REFILL_INTERVAL, 1000);
+        }
+
+        [Fact]
+        public void ShouldThrottle_WhenCalledWithNTokensLessThanMax_ReturnsFalse()
+        {
+            int waitTime;
+            var shouldThrottle = bucket.ShouldThrottle(N_LESS_THAN_MAX, out waitTime);
+
+            shouldThrottle.Should().BeFalse();
+            bucket.CurrentTokenCount.Should().Be(MAX_TOKENS - N_LESS_THAN_MAX);
+        }
+
+        [Fact]
+        public void ShouldThrottle_WhenCalledWithNTokensGreaterThanMax_ReturnsTrue()
+        {
+            int waitTime;
+            var shouldThrottle = bucket.ShouldThrottle(N_GREATER_THAN_MAX, out waitTime);
+
+            shouldThrottle.Should().BeTrue();
+            bucket.CurrentTokenCount.Should().Be(MAX_TOKENS);
+        }
+
+        [Fact]
+        public void ShouldThrottle_WhenCalledCumulativeNTimesIsLessThanMaxTokens_ReturnsFalse()
+        {
+            for (int i = 0; i < CUMULATIVE; i++)
+            {
+                bucket.ShouldThrottle(N_LESS_THAN_MAX).Should().BeFalse();
+            }
+
+            var tokens = bucket.CurrentTokenCount;
+
+            tokens.Should().Be(MAX_TOKENS - (CUMULATIVE*N_LESS_THAN_MAX));
+        }
+
+        [Fact]
+        public void ShouldThrottle_WhenCalledCumulativeNTimesIsGreaterThanMaxTokens_ReturnsTrue()
+        {
+            for (int i = 0; i < CUMULATIVE; i++)
+            {
+                bucket.ShouldThrottle(N_GREATER_THAN_MAX).Should().BeTrue();
+            }
+
+            var tokens = bucket.CurrentTokenCount;
+
+            tokens.Should().Be(MAX_TOKENS);
+        }
+
+        [Fact]
+        public void ShouldThrottle_WhenCalledWithNLessThanMaxSleepNLessThanMax_ReturnsFalse()
+        {
+            SystemTime.SetCurrentTimeUtc = () => new DateTime(2014, 2, 27, 0, 0, 0, DateTimeKind.Utc);
+            var virtualNow = SystemTime.UtcNow;
+
+            var before = bucket.ShouldThrottle(N_LESS_THAN_MAX);
+            var tokensBefore = bucket.CurrentTokenCount;
+            before.Should().BeFalse();
+            tokensBefore.Should().Be(MAX_TOKENS - N_LESS_THAN_MAX);
+
+            SystemTime.SetCurrentTimeUtc = () => virtualNow.AddSeconds(REFILL_INTERVAL);
+
+            var after = bucket.ShouldThrottle(N_LESS_THAN_MAX);
+            var tokensAfter = bucket.CurrentTokenCount;
+
+            after.Should().BeFalse();
+            tokensAfter.Should().Be(MAX_TOKENS - N_LESS_THAN_MAX);
+        }
+
+        [Fact]
+        public void ShouldThrottle_WhenCalledWithNGreaterThanMaxSleepNGreaterThanMax_ReturnsTrue()
+        {
+            SystemTime.SetCurrentTimeUtc = () => new DateTime(2014, 2, 27, 0, 0, 0, DateTimeKind.Utc);
+            var virtualNow = SystemTime.UtcNow;
+
+            var before = bucket.ShouldThrottle(N_GREATER_THAN_MAX);
+            var tokensBefore = bucket.CurrentTokenCount;
+
+            before.Should().BeTrue();
+            tokensBefore.Should().Be(MAX_TOKENS);
+
+            SystemTime.SetCurrentTimeUtc = () => virtualNow.AddSeconds(REFILL_INTERVAL);
+
+            var after = bucket.ShouldThrottle(N_GREATER_THAN_MAX);
+            var tokensAfter = bucket.CurrentTokenCount;
+            after.Should().BeTrue();
+            tokensAfter.Should().Be(MAX_TOKENS);
+        }
+
+        [Fact]
+        public void ShouldThrottle_WhenCalledWithNLessThanMaxSleepCumulativeNLessThanMax()
+        {
+            SystemTime.SetCurrentTimeUtc = () => new DateTime(2014, 2, 27, 0, 0, 0, DateTimeKind.Utc);
+            var virtualNow = SystemTime.UtcNow;
+
+            long sum = 0;
+            for (int i = 0; i < CUMULATIVE; i++)
+            {
+                bucket.ShouldThrottle(N_LESS_THAN_MAX).Should().BeFalse();
+                sum += N_LESS_THAN_MAX;
+            }
+            var tokensBefore = bucket.CurrentTokenCount;
+            tokensBefore.Should().Be(MAX_TOKENS - sum);
+
+            SystemTime.SetCurrentTimeUtc = () => virtualNow.AddSeconds(REFILL_INTERVAL);
+
+            for (int i = 0; i < CUMULATIVE; i++)
+            {
+                bucket.ShouldThrottle(N_LESS_THAN_MAX).Should().BeFalse();
+            }
+            var tokensAfter = bucket.CurrentTokenCount;
+            tokensAfter.Should().Be(MAX_TOKENS - sum);
+        }
+
+        [Fact]
+        public void ShouldThrottle_WhenCalledWithCumulativeNLessThanMaxSleepCumulativeNGreaterThanMax()
+        {
+            SystemTime.SetCurrentTimeUtc = () => new DateTime(2014, 2, 27, 0, 0, 0, DateTimeKind.Utc);
+            var virtualNow = SystemTime.UtcNow;
+
+            long sum = 0;
+            for (int i = 0; i < CUMULATIVE; i++)
+            {
+                bucket.ShouldThrottle(N_LESS_THAN_MAX).Should().BeFalse();
+                sum += N_LESS_THAN_MAX;
+            }
+            var tokensBefore = bucket.CurrentTokenCount;
+            tokensBefore.Should().Be(MAX_TOKENS - sum);
+
+            SystemTime.SetCurrentTimeUtc = () => virtualNow.AddSeconds(REFILL_INTERVAL);
+
+            for (int i = 0; i < 3*CUMULATIVE; i++)
+            {
+                bucket.ShouldThrottle(N_LESS_THAN_MAX);
+            }
+
+            var after = bucket.ShouldThrottle(N_LESS_THAN_MAX);
+            var tokensAfter = bucket.CurrentTokenCount;
+
+            after.Should().BeTrue();
+            tokensAfter.Should().BeLessThan(N_LESS_THAN_MAX);
+        }
+
+        [Fact]
+        public void ShouldThrottle_WhenCalledWithCumulativeNGreaterThanMaxSleepCumulativeNLessThanMax()
+        {
+            SystemTime.SetCurrentTimeUtc = () => new DateTime(2014, 2, 27, 0, 0, 0, DateTimeKind.Utc);
+            var virtualNow = SystemTime.UtcNow;
+
+            for (int i = 0; i < 3*CUMULATIVE; i++)
+                bucket.ShouldThrottle(N_LESS_THAN_MAX);
+
+            var before = bucket.ShouldThrottle(N_LESS_THAN_MAX);
+            var tokensBefore = bucket.CurrentTokenCount;
+
+            before.Should().BeTrue();
+            tokensBefore.Should().BeLessThan(N_LESS_THAN_MAX);
+
+            SystemTime.SetCurrentTimeUtc = () => virtualNow.AddSeconds(REFILL_INTERVAL);
+
+            long sum = 0;
+            for (int i = 0; i < CUMULATIVE; i++)
+            {
+                bucket.ShouldThrottle(N_LESS_THAN_MAX).Should().BeFalse();
+                sum += N_LESS_THAN_MAX;
+            }
+
+            var tokensAfter = bucket.CurrentTokenCount;
+            tokensAfter.Should().Be(MAX_TOKENS - sum);
+        }
+
+        [Fact]
+        public void ShouldThrottle_WhenCalledWithCumulativeNGreaterThanMaxSleepCumulativeNGreaterThanMax()
+        {
+            SystemTime.SetCurrentTimeUtc = () => new DateTime(2014, 2, 27, 0, 0, 0, DateTimeKind.Utc);
+            var virtualNow = SystemTime.UtcNow;
+
+            for (int i = 0; i < 3*CUMULATIVE; i++)
+                bucket.ShouldThrottle(N_LESS_THAN_MAX);
+
+            var before = bucket.ShouldThrottle(N_LESS_THAN_MAX);
+            var tokensBefore = bucket.CurrentTokenCount;
+
+            before.Should().BeTrue();
+            tokensBefore.Should().BeLessThan(N_LESS_THAN_MAX);
+
+            SystemTime.SetCurrentTimeUtc = () => virtualNow.AddSeconds(REFILL_INTERVAL);
+
+            for (int i = 0; i < 3*CUMULATIVE; i++)
+            {
+                bucket.ShouldThrottle(N_LESS_THAN_MAX);
+            }
+            var after = bucket.ShouldThrottle(N_LESS_THAN_MAX);
+            var tokensAfter = bucket.CurrentTokenCount;
+
+            after.Should().BeTrue();
+            tokensAfter.Should().BeLessThan(N_LESS_THAN_MAX);
+        }
+
+        [Fact]
+        public void ShouldThrottle_WhenThread1NLessThanMaxAndThread2NLessThanMax()
+        {
+            var t1 = new Thread(p =>
+            {
+                var throttle = bucket.ShouldThrottle(N_LESS_THAN_MAX);
+                throttle.Should().BeFalse();
+            });            
+            
+            var t2 = new Thread(p =>
+            {
+                var throttle = bucket.ShouldThrottle(N_LESS_THAN_MAX);
+                throttle.Should().BeFalse();
+            });
+
+            t1.Start();
+            t2.Start();
+
+            t1.Join();
+            t2.Join();
+
+            bucket.CurrentTokenCount.Should().Be(MAX_TOKENS - 2*N_LESS_THAN_MAX);
+        }
+        
+        [Fact]
+        public void ShouldThrottle_Thread1NGreaterThanMaxAndThread2NGreaterThanMax()
+        {
+            var shouldThrottle = bucket.ShouldThrottle(N_GREATER_THAN_MAX);
+            shouldThrottle.Should().BeTrue();
+
+            var t1 = new Thread(p =>
+            {
+                var throttle = bucket.ShouldThrottle(N_GREATER_THAN_MAX);
+                throttle.Should().BeTrue();
+            });            
+            
+            var t2 = new Thread(p =>
+            {
+                var throttle = bucket.ShouldThrottle(N_GREATER_THAN_MAX);
+                throttle.Should().BeTrue();
+            });
+
+            t1.Start();
+            t2.Start();
+
+            t1.Join();
+            t2.Join();
+
+            bucket.CurrentTokenCount.Should().Be(MAX_TOKENS);
+        }
+    }
+}
---
+++ src/LimitsMiddleware.Tests/RateLimiters/RollingWindowThrottlerTests.cs
@@ -0,0 +1,229 @@
+﻿using System;
+
+namespace Bert.RateLimiters.Tests
+{
+    using FluentAssertions;
+    using Xunit;
+
+    public class RollingWindowThrottlerTests
+    {
+        private readonly DateTime referenceTime = new DateTime(2014, 9, 20, 0, 0, 0, DateTimeKind.Utc);
+
+        [Fact]
+        public void Throws_WhenNumberOfOccurencesIsLesserThanOne()
+        {
+            Action act = () => new RollingWindowThrottler(-1, TimeSpan.FromSeconds(5));
+
+            act.ShouldThrow<ArgumentOutOfRangeException>();
+        }
+
+        [Fact]
+        public void ShouldThrottle_WhenCalledWithTokensLessThanOne_WillThrow()
+        {
+            var throttler = new RollingWindowThrottler(1, TimeSpan.FromSeconds(1));
+            long waitTimeMillis;
+
+            Action act = () => throttler.ShouldThrottle(0, out waitTimeMillis);
+
+            act.ShouldThrow<ArgumentOutOfRangeException>();
+        }
+
+        [Fact]
+        public void ShouldThrottle_WhenCalled_WillReturnFalse()
+        {
+            var throttler = new RollingWindowThrottler(1, TimeSpan.FromSeconds(1));
+            long waitTimeMillis;
+            var shouldThrottle = throttler.ShouldThrottle(1, out waitTimeMillis);
+
+            shouldThrottle.Should().BeFalse();
+        }
+
+        [Fact]
+        public void ShouldThrottle_WhenCalledTwiceinSameSecondAndAllows1PerSecond_WillReturnTrue()
+        {
+
+            SystemTime.SetCurrentTimeUtc = () => referenceTime;
+            var virtualNow = SystemTime.UtcNow;
+
+            var throttler = new RollingWindowThrottler(1, TimeSpan.FromSeconds(1));
+            long waitTimeMillis;
+            var shouldThrottle = throttler.ShouldThrottle(1, out waitTimeMillis);
+            shouldThrottle.Should().BeFalse();
+
+            SystemTime.SetCurrentTimeUtc = () => virtualNow.AddSeconds(0.5);
+            shouldThrottle = throttler.ShouldThrottle(1, out waitTimeMillis);
+            shouldThrottle.Should().BeTrue();
+            waitTimeMillis.Should().Be(500);
+
+            SystemTime.SetCurrentTimeUtc = () => virtualNow.AddSeconds(0.8);
+            shouldThrottle = throttler.ShouldThrottle(1, out waitTimeMillis);
+            shouldThrottle.Should().BeTrue();
+            waitTimeMillis.Should().Be(200);
+        }
+
+
+        [Fact]
+        public void ShouldThrottle_WhenCalledAfterSecondPassAndAllows1PerSecond_WillReturnFalse()
+        {
+
+            SystemTime.SetCurrentTimeUtc = () => referenceTime;
+            var virtualNow = SystemTime.UtcNow;
+
+            var throttler = new RollingWindowThrottler(1, TimeSpan.FromSeconds(1));
+            long waitTimeMillis;
+            var shouldThrottle = throttler.ShouldThrottle(1, out waitTimeMillis);
+            shouldThrottle.Should().BeFalse();
+
+            SystemTime.SetCurrentTimeUtc = () => virtualNow.AddSeconds(1);
+            shouldThrottle = throttler.ShouldThrottle(1, out waitTimeMillis);
+            shouldThrottle.Should().BeFalse();
+        }        
+        
+        [Fact]
+        public void ShouldThrottle_WhenCalledTwiceinSameSecondAndAllows2PerSecond_WillReturnFalse()
+        {
+
+            SystemTime.SetCurrentTimeUtc = () => referenceTime;
+            var virtualNow = SystemTime.UtcNow;
+
+            var throttler = new RollingWindowThrottler(2, TimeSpan.FromSeconds(1));
+            long waitTimeMillis;
+            var shouldThrottle = throttler.ShouldThrottle(1, out waitTimeMillis);
+            shouldThrottle.Should().BeFalse();
+
+            SystemTime.SetCurrentTimeUtc = () => virtualNow.AddSeconds(0.5);
+            shouldThrottle = throttler.ShouldThrottle(1, out waitTimeMillis);
+            shouldThrottle.Should().BeFalse();
+        }
+
+        [Fact]
+        public void ShouldThrottle_WhenCalledAfterSecondPassesAndAllows2PerSecond_WillReturnFalse()
+        {
+
+            SystemTime.SetCurrentTimeUtc = () => referenceTime;
+            var virtualNow = SystemTime.UtcNow;
+
+            var throttler = new RollingWindowThrottler(2, TimeSpan.FromSeconds(1));
+            long waitTimeMillis;
+            var shouldThrottle = throttler.ShouldThrottle(1, out waitTimeMillis);
+            shouldThrottle.Should().BeFalse();
+
+            SystemTime.SetCurrentTimeUtc = () => virtualNow.AddSeconds(1);
+            shouldThrottle = throttler.ShouldThrottle(1, out waitTimeMillis);
+            shouldThrottle.Should().BeFalse();
+            waitTimeMillis.Should().Be(0);
+        }
+
+        [Fact]
+        public void ShouldThrottle_WhenCalledThreeTimesinSameSecondAndAllows2PerSecond_WillReturnTrue()
+        {
+
+            SystemTime.SetCurrentTimeUtc = () => referenceTime;
+            var virtualNow = SystemTime.UtcNow;
+
+            var throttler = new RollingWindowThrottler(2, TimeSpan.FromSeconds(1));
+            long waitTimeMillis;
+            var shouldThrottle = throttler.ShouldThrottle(1, out waitTimeMillis);
+            shouldThrottle.Should().BeFalse();
+
+            SystemTime.SetCurrentTimeUtc = () => virtualNow.AddSeconds(0.5);
+            shouldThrottle = throttler.ShouldThrottle(1, out waitTimeMillis);
+            shouldThrottle.Should().BeFalse();
+
+            SystemTime.SetCurrentTimeUtc = () => virtualNow.AddSeconds(0.7);
+            shouldThrottle = throttler.ShouldThrottle(1, out waitTimeMillis);
+            shouldThrottle.Should().BeTrue();
+            waitTimeMillis.Should().Be(300);
+
+        }
+
+
+        [Fact]
+        public void ShouldThrottle_WhenCalledAtEndOfRollingWindowAndAllows2PerSecond_WillReturnFalse()
+        {
+
+            SystemTime.SetCurrentTimeUtc = () => referenceTime;
+            var virtualNow = SystemTime.UtcNow;
+
+            var throttler = new RollingWindowThrottler(2, TimeSpan.FromSeconds(1));
+            long waitTimeMillis;
+            var shouldThrottle = throttler.ShouldThrottle(1, out waitTimeMillis);
+            shouldThrottle.Should().BeFalse();
+
+            //first rolling window expired, init a new one
+            SystemTime.SetCurrentTimeUtc = () => virtualNow.AddSeconds(1.2);
+            shouldThrottle = throttler.ShouldThrottle(1, out waitTimeMillis);
+            shouldThrottle.Should().BeFalse();
+
+            //inside second rolling window, under threshold
+            SystemTime.SetCurrentTimeUtc = () => virtualNow.AddSeconds(1.3);
+            shouldThrottle = throttler.ShouldThrottle(1, out waitTimeMillis);
+            shouldThrottle.Should().BeFalse();
+
+            //second rolling window expired, beginning third window
+            SystemTime.SetCurrentTimeUtc = () => virtualNow.AddSeconds(2.2);
+            shouldThrottle = throttler.ShouldThrottle(1, out waitTimeMillis);
+            shouldThrottle.Should().BeFalse();
+
+            //third window, under threshold
+            SystemTime.SetCurrentTimeUtc = () => virtualNow.AddSeconds(2.3);
+            shouldThrottle = throttler.ShouldThrottle(1, out waitTimeMillis);
+            shouldThrottle.Should().BeFalse();
+
+        }
+
+        [Fact]
+        public void ShouldThrottle_WhenCalledWithMoreTokensThanOccurrences_WillReturnTrue()
+        {
+            var throttler = new RollingWindowThrottler(2, TimeSpan.FromSeconds(1));
+            long waitTimeMillis;
+            var shouldThrottle = throttler.ShouldThrottle(3, out waitTimeMillis);
+            shouldThrottle.Should().BeTrue();            
+        }
+
+        [Fact]
+        public void ShouldThrottle_WhenCalledAndConsumingAllTokensAtOnce_WillReturnFalse()
+        {
+            var throttler = new RollingWindowThrottler(3, TimeSpan.FromSeconds(1));
+            long waitTimeMillis;
+            var shouldThrottle = throttler.ShouldThrottle(3, out waitTimeMillis);
+            shouldThrottle.Should().BeFalse();
+        }
+
+        [Fact]
+        public void ShouldThrottle_WhenCalledAndConsumingAllTokensAtOnceAndThenCalledOnceMore_WillReturnTrue()
+        {
+            SystemTime.SetCurrentTimeUtc = () => referenceTime;
+            var virtualNow = SystemTime.UtcNow;
+
+            var throttler = new RollingWindowThrottler(3, TimeSpan.FromSeconds(1));
+            long waitTimeMillis;
+            var shouldThrottle = throttler.ShouldThrottle(3, out waitTimeMillis);
+            shouldThrottle.Should().BeFalse();
+
+            SystemTime.SetCurrentTimeUtc = () => virtualNow.AddSeconds(0.2);
+            shouldThrottle = throttler.ShouldThrottle(3, out waitTimeMillis);
+            shouldThrottle.Should().BeTrue();
+            waitTimeMillis.Should().Be(800);
+        }
+
+
+        [Fact]
+        public void ShouldThrottle_WhenCalledAndConsumingAllTokensAtOnceAndThenCalledOnceMoreAfterRollingWindowEnd_WillReturnFalse()
+        {
+            SystemTime.SetCurrentTimeUtc = () => referenceTime;
+            var virtualNow = SystemTime.UtcNow;
+
+            var throttler = new RollingWindowThrottler(3, TimeSpan.FromSeconds(1));
+            long waitTimeMillis;
+            var shouldThrottle = throttler.ShouldThrottle(3, out waitTimeMillis);
+            shouldThrottle.Should().BeFalse();
+
+            SystemTime.SetCurrentTimeUtc = () => virtualNow.AddSeconds(1.1);
+            shouldThrottle = throttler.ShouldThrottle(3, out waitTimeMillis);
+            shouldThrottle.Should().BeFalse();
+            waitTimeMillis.Should().Be(0);
+        }
+
+    }
+}
