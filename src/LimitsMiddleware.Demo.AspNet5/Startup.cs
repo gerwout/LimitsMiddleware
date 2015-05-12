bd6e4621c3df4d386f228bff0f6001d8432bc6e2
Damian Hickey <dhickey@gmail.com>
2015-05-12 17:34:02 +0200

    Code cleanup

--- src/LimitsMiddleware.Demo.AspNet5/Startup.cs
+++ src/LimitsMiddleware.Demo.AspNet5/Startup.cs
@@ -1,26 +1,24 @@
-﻿using Microsoft.AspNet.Builder;
-using Microsoft.AspNet.Http;
-using Microsoft.Framework.DependencyInjection;
-
-namespace LimitsMiddleware.Demo.AspNet5
+﻿namespace LimitsMiddleware.Demo.AspNet5
 {
     using System;
     using System.Collections.Specialized;
     using System.IO;
+    using Microsoft.AspNet.Builder;
+    using Microsoft.AspNet.Http;
+    using Microsoft.Framework.DependencyInjection;
 
     public class Startup
     {
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
-        {
-        }
+        {}
 
         public void Configure(IApplicationBuilder app)
         {
             app.MaxUrlLength(100);
 
             app.MaxQueryStringLength(80);
-            
+
             app.MaxConcurrentRequests(4);
 
             app.MinResponseDelay(context =>
@@ -31,8 +29,8 @@ namespace LimitsMiddleware.Demo.AspNet5
                 return int.TryParse(minResponseDelayParam, out minResponseDelay)
                     ? TimeSpan.FromSeconds(minResponseDelay)
                     : TimeSpan.Zero;
-            });           
-           
+            });
+
             app.MaxBandwidthPerRequest(context =>
             {
                 var queryString = ParseQueryString(QueryString.FromUriComponent(context.Uri).Value);
@@ -43,17 +41,17 @@ namespace LimitsMiddleware.Demo.AspNet5
                     : -1;
             });
 
-            app.MaxBandwidthGlobal(10 * 1024 * 1024);
+            app.MaxBandwidthGlobal(10*1024*1024);
 
             app.Use(async (context, next) =>
             {
-                if (!context.Request.Path.Equals("/file", StringComparison.OrdinalIgnoreCase))
+                if(!context.Request.Path.Value.Equals("/file", StringComparison.OrdinalIgnoreCase))
                 {
                     await next();
                     return;
                 }
                 int size;
-                if (!int.TryParse(context.Request.Query.Get("size"), out size))
+                if(!int.TryParse(context.Request.Query.Get("size"), out size))
                 {
                     await next();
                     return;
@@ -63,21 +61,13 @@ namespace LimitsMiddleware.Demo.AspNet5
                 context.Response.ContentType = "application/octect-stream";
                 context.Response.StatusCode = 200;
                 var buffer = new byte[16384];
-                int count = 0;
-                while (count < size)
+                var count = 0;
+                while(count < size)
                 {
-                    int length = Math.Min(buffer.Length, size - count);
+                    var length = Math.Min(buffer.Length, size - count);
                     await context.Response.Body.WriteAsync(buffer, 0, length, context.RequestAborted);
                     count += length;
                 }
-            });
-
-            app.Use(async (context, next) =>
-            {
-                context.Response.ContentType = "text/html";
-                context.Response.StatusCode = 200;
-                var index = File.ReadAllText("index.html");
-                await context.Response.WriteAsync(index);
             });
         }
 
@@ -89,7 +79,10 @@ namespace LimitsMiddleware.Demo.AspNet5
             foreach(var pair in pairs)
             {
                 var nameAndValue = pair.Split('=');
-                result.Add(nameAndValue[0], nameAndValue[1]);
+                if(nameAndValue.Length == 2)
+                {
+                    result.Add(nameAndValue[0], nameAndValue[1]);
+                }
             }
             return result;
         }
